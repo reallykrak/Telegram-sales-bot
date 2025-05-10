b86cf203b079f9501d1194d42e1717d10b685f42
krak <notreallykrak@gmail.com>
2025-05-10 12:19:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,16 @@
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
-from handlers import start_handler, purchase_handler, order_history_handler, show_feedback_handler
-from admin_panel import admin_commands
-from menu import main_menu_handler
+from aiogram import Bot, Dispatcher, executor, types
+import config
+import keyboards
+import handlers
 
-app = ApplicationBuilder().token("7656355783:AAF7_PXEx8YeCNxCeJiqvgv8lTBzlkf6AIw").build()
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher(bot)
 
-# Kullanıcı komutları
-app.add_handler(CommandHandler("start", start_handler))
-app.add_handler(CallbackQueryHandler(main_menu_handler, pattern="menu"))
-app.add_handler(CallbackQueryHandler(order_history_handler, pattern="order_history"))
-app.add_handler(CallbackQueryHandler(show_feedback_handler, pattern="feedback"))
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await message.answer("Merhaba! Butona bas ve mesajı al!", reply_markup=keyboards.menu)
 
-# Admin işlemleri
-app.add_handler(admin_commands)
+handlers.register_handlers(dp)
 
-app.run_polling()
+if __name__ == "__main__":
+    executor.start_polling(dp)
