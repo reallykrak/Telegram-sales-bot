5c5b4ea0cea4c0bdb03ad4a2c6807ca2680ec846
krak <notreallykrak@gmail.com>
2025-05-10 12:04:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 from handlers import start_handler, purchase_handler, order_history_handler, show_feedback_handler
 from admin_panel import admin_commands
 from menu import main_menu_handler
 
-updater = Updater("7656355783:AAF7_PXEx8YeCNxCeJiqvgv8lTBzlkf6AIw")
-dispatcher = updater.dispatcher
+app = ApplicationBuilder().token("7656355783:AAF7_PXEx8YeCNxCeJiqvgv8lTBzlkf6AIw").build()
 
 # Kullanıcı komutları
-dispatcher.add_handler(CommandHandler("start", start_handler))
-dispatcher.add_handler(CallbackQueryHandler(main_menu_handler, pattern="menu"))
-dispatcher.add_handler(CallbackQueryHandler(order_history_handler, pattern="order_history"))
-dispatcher.add_handler(CallbackQueryHandler(show_feedback_handler, pattern="feedback"))
+app.add_handler(CommandHandler("start", start_handler))
+app.add_handler(CallbackQueryHandler(main_menu_handler, pattern="menu"))
+app.add_handler(CallbackQueryHandler(order_history_handler, pattern="order_history"))
+app.add_handler(CallbackQueryHandler(show_feedback_handler, pattern="feedback"))
 
 # Admin işlemleri
-dispatcher.add_handler(admin_commands)
+app.add_handler(admin_commands)
 
-updater.start_polling()
+app.run_polling()
