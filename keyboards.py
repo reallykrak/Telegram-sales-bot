3f663e49cfd0055145541a3ae4d12e476aa0fadd
krak <notreallykrak@gmail.com>
2025-05-10 12:39:38 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,5 +1,11 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-menu = InlineKeyboardMarkup()
-button = InlineKeyboardButton(text="Butona Bas", callback_data="clicked")
-menu.add(button)
+def main_menu():
+    """
+    Inline bir menü oluşturur.
+    Butona basıldığında callback_data "button_pressed" gönderilir.
+    """
+    keyboard = InlineKeyboardMarkup()
+    button = InlineKeyboardButton(text="Butona Bas", callback_data="button_pressed")
+    keyboard.add(button)
+    return keyboard
