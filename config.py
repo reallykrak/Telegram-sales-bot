ed777e1b2d823a90a18f327e012c37766831c59c
krak <notreallykrak@gmail.com>
2025-05-10 12:18:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,1 @@
-CONFIG = {
-    "BOT_TOKEN": "7656355783:AAF7_PXEx8YeCNxCeJiqvgv8lTBzlkf6AIw",
-    "ADMIN_IDS": ["12345678", "98765432"],  # Birden fazla admin ID eklenebilir
-}
+TOKEN = "TELEGRAM_BOT_TOKEN_BURAYA"
