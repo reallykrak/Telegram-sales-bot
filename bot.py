82a7bcf2283c89d905e3b4ea9d43c112b24905c2
krak <notreallykrak@gmail.com>
2025-05-10 12:40:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,12 @@
-from aiogram import Bot, Dispatcher, executor, types
+import telebot
 import config
-import keyboards
 import handlers
 
-bot = Bot(token=config.TOKEN)
-dp = Dispatcher(bot)
+# Bot nesnesini oluştur
+bot = telebot.TeleBot(config.TOKEN)
 
-@dp.message_handler(commands=["start"])
-async def start(message: types.Message):
-    await message.answer("Merhaba! Butona bas ve mesajı al!", reply_markup=keyboards.menu)
+# Handler’ları kayıt altına al
+handlers.register_handlers(bot)
 
-handlers.register_handlers(dp)
-
-if __name__ == "__main__":
-    executor.start_polling(dp)
+# Botu polling yöntemi ile çalıştır
+bot.polling()
